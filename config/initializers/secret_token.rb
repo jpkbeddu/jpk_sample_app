1234d43a07eeedfa02d3aefd7b18a51929c30ad0
jpkbeddu <jpkbeddu@gmail.com>
2013-08-24 02:05:47 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JpkApp::Application.config.secret_token = '04429cd8d9ecd6d97fe7644a70ed9210ba71ca589d17ffba8910d67d25a80a926b409f665a216a3fbabff51d5139fdd753531fee710c18f39912b94f7068f1ac'
